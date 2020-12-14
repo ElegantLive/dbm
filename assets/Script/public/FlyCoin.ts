3cadde15bbcc80a33158f862c06a048feba0a48a
caixian <qq52577517@163.com>
2020-12-14 15:24:54 +0800

    优化金币墙动作

--- assets/Script/public/FlyCoin.ts
+++ assets/Script/public/FlyCoin.ts
@@ -1,5 +1,5 @@
+import TipsModal from "../game/TipsModal";
 import { increaseCoin } from "../state/User";
-import AudioManager from "./AudioManager";
 
 const { ccclass, property } = cc._decorator;
 
@@ -12,7 +12,7 @@ export default class FlyCoin extends cc.Component {
   init(node: cc.Node, container: cc.Node) {
     const ws = node.convertToWorldSpaceAR(cc.v2(0, 0));
     const pos = container.convertToNodeSpaceAR(ws);
-    pos.y += node.height / 2;
+    pos.y += node.height;
     this.node.setPosition(pos);
     cc.find("Canvas").addChild(this.node);
     cc.find("root").getComponent("AudioManager").playOnceMusic("coin");
@@ -26,17 +26,17 @@ export default class FlyCoin extends cc.Component {
       act = {
         scaleX: -1,
       };
-    let up = cc.tween().to(0.3, act),
-      down = cc.tween().to(0.3, dft),
+    let up = cc.tween().to(0.2, act),
+      down = cc.tween().to(0.4, dft),
       scaleAction = cc.tween().then(up).then(down),
       upAction = cc.tween().to(0.6, {
-        y: this.node.y + 80,
-        opacity: 0,
+        y: this.node.y + this.node.height * 2,
       });
     cc.tween(this.node)
       .parallel(scaleAction, upAction)
       .call(() => {
         increaseCoin(1);
+        this.node.opacity = 0;
         this.node.destroy();
       })
       .start();
